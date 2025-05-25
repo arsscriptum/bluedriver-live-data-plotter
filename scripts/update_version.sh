b8964112409167d4c341c3117d21405b20d4cfa3
Guillaume Plante <tigui.plante@icloud.com>
2025-05-25 13:43:41 -0400

    Updated Version and version scripts

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -5,13 +5,25 @@ pecho() { printf "%s\n" "$*"; }
 
 log() { pecho "$@"; }
 
-
+APPLICATION_TITLE="BlueDriver Live Statistics Viewer"
 SCRIPT_PATH=$(realpath "$BASH_SOURCE")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+BUILD_USER=$(whoami)
+BUILD_TYPE="dev"
+BUILD_DATE=$(date +"%Y-%m-%d %H:%M")
+
+
+
+if [[ $BUILD_USER == "runner" ]]; then
+    BUILD_TYPE="official"
+fi
+
+
 
 tmp_root=$(pushd "$SCRIPT_DIR/.." | awk '{print $1}')
 ROOT_DIR=$(eval echo "$tmp_root")
-ENV_FILE="$ROOT_DIR/.env"
+# Path to output site.json
+SITE_JSON="$ROOT_DIR/html/site.json"
 
 LOGS_DIR="$ROOT_DIR/logs"
 LOG_FILE="$LOGS_DIR/build.log"
@@ -41,6 +53,21 @@ last_rev=$(git log --format=%h -2 | tail -n 1)
     echo "$head_rev"
 } > "$BUILD_FILE"
 
-
+log "Build date is $BUILD_DATE"
+log "Build user is $BUILD_USER"
+log "Build type is $BUILD_TYPE"
 log "Version updated to $new_version"
 log "Branch and revision info saved to $BUILD_FILE"
+
+# Write JSON file
+cat > "$SITE_JSON" <<EOF
+{
+  "Title": "$APPLICATION_TITLE",
+  "Version": "$new_version",
+  "Branch": "$current_branch",
+  "Revision": "$head_rev-$BUILD_TYPE",
+  "BuiltOn": "$BUILD_DATE"
+}
+EOF
+
+log "Generated site.json at $SITE_JSON"
